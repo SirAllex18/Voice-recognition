0f2b9d56a5be7bc53fe83386046a267b0dd82fd5
SirAllex <anistor35@yahoo.com>
2025-01-07 01:52:50 +0200

    HomePage component structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/index"
 
 function App() {
   return (
     <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ client/src/scenes/index.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return(
+        <>
+        <div>Hello Site </div>
+        </>
+    )
+}
+
+export default HomePage;
