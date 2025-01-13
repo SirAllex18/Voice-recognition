815255666ae30782b222f6b5c6c26217ab6463f3
SirAllex18 <anistor35@yahoo.com>
2025-01-13 14:50:01 +0200

    added login possibility, logout and save the User info

--- client/src/scenes/index.jsx
+++ client/src/scenes/index.jsx
@@ -10,26 +10,32 @@ import {
   DialogContent,
   DialogActions,
   TextField,
-  CircularProgress
+  CircularProgress,
 } from "@mui/material";
 
 const HomePage = () => {
-  const navigate = useNavigate();
   const [isLogin, setLogin] = useState(false);
   const [openDialog, setOpenDialog] = useState(false);
   const [formData, setFormData] = useState({
     nume: "",
     prenume: "",
     parola: "",
+    mail: "",
   });
   const [recording, setRecording] = useState(false);
+  const [alreadyRegistered, setRegistered] = useState(false);
   const [audioUrl, setAudioUrl] = useState(null);
   const [isUploading, setIsUploading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
-  
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
   let mediaRecorder;
   let audioChunks = [];
   const startRecording = () => {
+    if (isLogin === false) {
+      setOpenDialog(true);
+      return;
+    }
     setRecording(true);
     setErrorMessage(null);
 
@@ -56,7 +62,6 @@ const HomePage = () => {
           uploadAudio(audioFile);
         };
 
-        // Stop recording after 3-5 seconds
         setTimeout(() => {
           mediaRecorder.stop();
           setRecording(false);
@@ -71,7 +76,7 @@ const HomePage = () => {
   const uploadAudio = (audioFile) => {
     setIsUploading(true);
     const formData = new FormData();
-    formData.append("audio", audioFile);
+    formData.append("file", audioFile);
 
     fetch("http://localhost:8000/recognize", {
       method: "POST",
@@ -98,26 +103,94 @@ const HomePage = () => {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
-  const handleSubmitForm = () => {
-    console.log("Form Submitted:", formData);
-    setOpenDialog(false);
+  const handleLoginButton = () => {
+    setRegistered(true);
   };
+
+  const handleLogout = () => {
+    setLoggedInUser(null);
+    setLogin(false);
+    alert("You have been logged out");
+  };
+
+  const handleSubmitForm = async () => {
+    if (!alreadyRegistered) {
+      try {
+        const response = await fetch("http://localhost:8000/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+          alert("Registration successful!");
+          setLoggedInUser(data);
+          setOpenDialog(false);
+          setLogin(true);
+        } else {
+          console.error("Error:", data.detail);
+          alert(data.detail);
+        }
+      } catch (error) {
+        console.error("Error during registration:", error);
+        alert("Something went wrong!");
+      }
+    } else {
+      try {
+        const formDataLogin = new FormData();
+        formDataLogin.append("email", formData.mail);
+        formDataLogin.append("password", formData.password);
+
+        const response = await fetch("http://localhost:8000/login", {
+          method: "POST",
+          body: formDataLogin,
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+          alert("Login successful!");
+          setLoggedInUser(data);
+          localStorage.setItem("token", data.access_token);
+          setOpenDialog(false);
+          setLogin(true);
+        } else {
+          console.error("Error:", data.detail);
+          alert(data.detail);
+        }
+      } catch (error) {
+        console.error("Error during login:", error);
+        alert("Something went wrong!");
+      }
+    }
+  };
+  console.log('Here is userdata', loggedInUser);
 
   return (
     <>
-      <Typography variant="h5" align="center" gutterBottom>
-        Your personal Voice Authentication Application
+      <Typography variant="h4" align="center" marginTop="8rem">
+        Your personal voice authentication application
       </Typography>
+      {isLogin && (
+        <Typography variant="body1" align="center">
+          Hello, {loggedInUser.user.prenume}
+      </Typography>
+      )}
+      
       <Container
         sx={{
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          height: "100vh",
+          height: "auto",
         }}
       >
-        <Typography variant="body2" align="center" gutterBottom>
+        <Typography variant="body1" align="center" gutterBottom>
           Choose your usage:
         </Typography>
         <Box
@@ -126,7 +199,9 @@ const HomePage = () => {
             borderRadius: 2,
             padding: 4,
             width: "300px",
-            textAlign: "center",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
             boxShadow: 2,
           }}
         >
@@ -143,24 +218,31 @@ const HomePage = () => {
               color="primary"
               onClick={startRecording}
               disabled={recording}
-              style={{ marginTop: "20px" }}
+              style={{ width: "10rem" }}
             >
-              {recording ? "Recording..." : "Start Recording"}
+              {recording ? "Recording..." : "Authentication"}
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={startRecording}
+              disabled={recording}
+              style={{ width: "10rem" }}
+            >
+              {recording ? "Recording..." : "Recognition"}
             </Button>
             {recording && <CircularProgress style={{ marginTop: "20px" }} />}
-            {audioUrl && (
-              <audio
-                controls
-                src={audioUrl}
-                style={{ display: "block", marginTop: "20px" }}
-              />
-            )}
             {isUploading && <Typography>Uploading...</Typography>}
             {errorMessage && (
               <Typography color="error">{errorMessage}</Typography>
             )}
           </Box>
         </Box>
+        {isLogin && (
+          <Box>
+            <Button onClick={handleLogout}> Logout </Button>
+          </Box>
+        )}
       </Container>
 
       {/* Dialog */}
@@ -174,30 +256,76 @@ const HomePage = () => {
             paddingTop: 2,
           }}
         >
-          <TextField
-            label="Nume"
-            name="nume"
-            value={formData.nume}
-            onChange={handleInputChange}
-          />
-          <TextField
-            label="Prenume"
-            name="prenume"
-            value={formData.prenume}
-            onChange={handleInputChange}
-          />
-          <TextField
-            label="Parola"
-            name="parola"
-            type="password"
-            value={formData.parola}
-            onChange={handleInputChange}
-          />
+          {!alreadyRegistered && (
+            <>
+              <TextField
+                label="Nume"
+                name="nume"
+                value={formData.nume}
+                onChange={handleInputChange}
+              />
+              <TextField
+                label="Prenume"
+                name="prenume"
+                value={formData.prenume}
+                onChange={handleInputChange}
+              />
+              <TextField
+                label="E-mail"
+                name="mail"
+                value={formData.mail}
+                onChange={handleInputChange}
+              />
+              <TextField
+                label="Parola"
+                name="password"
+                type="password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+            </>
+          )}
+
+          {alreadyRegistered && (
+            <>
+              <TextField
+                label="E-mail"
+                name="mail"
+                value={formData.mail}
+                onChange={handleInputChange}
+              />
+              <TextField
+                label="Parola"
+                name="password"
+                type="password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+            </>
+          )}
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog}>Cancel</Button>
-          <Button variant="contained" onClick={handleSubmitForm}>
-            Submit
+        <DialogActions
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              gap: 2,
+              justifyContent: "center",
+              width: "100%",
+            }}
+          >
+            <Button onClick={handleCloseDialog}>Cancel</Button>
+            <Button variant="contained" onClick={handleSubmitForm}>
+              Submit
+            </Button>
+          </Box>
+          <Button variant="text" onClick={handleLoginButton}>
+            Already registered?
           </Button>
         </DialogActions>
       </Dialog>
